cd8d69f0d87ebb295860b5c9be539bf5c6449c1e
Santona Tuli <stuli@ucdavis.edu>
2019-10-22 16:02:45 -0700

    Refactoring the analysis software

--- Efficiency_Old/runEffsys.sh
+++
@@ -1,3 +0,0 @@
-root -l -b -q 'getEffSys.C+(1)'
-root -l -b -q 'getEffSys.C+(2)'
-root -l -b -q 'getEffSys.C+(3)'
--- UnusedCode/run_getSpectra.sh
+++
@@ -1,3 +0,0 @@
-root -l -b -q 'getSpectra.C+(1)'
-root -l -b -q 'getSpectra.C+(2)'
-root -l -b -q 'getSpectra.C+(3)'
